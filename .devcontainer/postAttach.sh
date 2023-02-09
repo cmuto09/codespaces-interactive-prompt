e7e6d0dea6137e6e428e50867ae4f6f881dadaae
cmuto09 <cmuto09@github.com>
2023-02-09 21:05:47 +0000

    working on gh token

--- .devcontainer/postAttach.sh
+++ .devcontainer/postAttach.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+unset GITHUB_TOKEN
+gh auth login -s codespace:secrets
+
 echo "What is your name?"
 read name
 echo "$name, what is your favorite color?"
 read favorite_color
 echo "See ya!"
+echo "$name, should I show you your secrets?"
+read view_secrets
+if [[ $view_secrets == "y" ]]; then
+    echo "Cool cool cool"
+    gh secret list
+else
+    echo "Fine, don't tell me."
+fi
